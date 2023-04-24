090506f1a4d630dfbfd6cceed4f1462c105dbf92
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-04-24 21:41:38 +0200

    Initial commit

---
+++ include/IDASignatureScanner.hpp
@@ -0,0 +1,17 @@
+#ifndef IDASIGNATURESCANNER
+#define IDASIGNATURESCANNER
+
+#include <string>
+#include <vector>
+
+typedef std::pair<unsigned char, bool> Byte;
+typedef std::vector<Byte> Signature;
+
+namespace SignatureScanner {
+	Signature BuildSignature(const std::string& str);
+	bool DoesMatch(const Signature& signature, void* address);
+	void* FindNextOccurrence(const Signature& signature, void* begin);
+	std::vector<void*> FindAllOccurrences(const Signature& signature, void* begin, void* end);
+}
+
+#endif
