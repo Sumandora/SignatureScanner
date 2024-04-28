cdf3949ead3c42528a4894b8a0cf8b5296d76671
johannes <johannesmiesenhardt@gmail.com>
2024-04-28 22:51:35 +0200

    Rewrite to single header, add support for iterators and custom endianness,

---
+++ Example/Source/Main.cpp
@@ -0,0 +1,227 @@
+#include "SignatureScanner.hpp"
+
+#include <span>
+#include <cstring>
+#include <gtest/gtest.h>
+
+using namespace SignatureScanner;
+
+static std::uint8_t bytes[]{
+	0x68, 0x74, 0x16, 0xcd, 0xaa, 0xe3, 0x6, 0x95, 0xcb, 0xeb, 0xe7,
+	0x64, 0x1e, 0xbb, 0x5a, 0xf2, 0x65, 0xe5, 0x53, 0x85, 0xb8,
+	0xfe, 0xb4, 0x3f, 0xb4, 0x38, 0x3a, 0x1a, 0xc4, 0x5f, 0x00,
+	0x5e, 0x35, 0xe7, 0xd4, 0x3d, 0xb3, 0x51, 0x98, 0xa7, 0x66,
+	0x1d, 0xe4, 0xff, 0x9a, 0x63, 0xa, 0x37, 0x6f, 0xd, 0x24,
+	0xa9, 0x5c, 0x19, 0xb9, 0xa1, 0xfb, 0x91, 0x73, 0xd7, 0x3d,
+	0xc, 0x9b, 0xb, 0xac, 0xd2, 0x49, 0x98, 0x2d, 0x8, 0x29,
+	0xb6, 0xf0, 0x43, 0xe4, 0x7, 0x5, 0xfa, 0x30, 0x81, 0xc9,
+	0xad, 0xaf, 0x7c, 0x8, 0xee, 0xca, 0xdf, 0xdb, 0x2c, 0x76,
+	0xa9, 0x49, 0xb8, 0xf5, 0xcd, 0x4d, 0xa9, 0x14, 0xc0, 0xaf
+};
+std::span<std::byte> bytesSpan{ reinterpret_cast<std::byte*>(bytes), sizeof(bytes) };
+
+TEST(BytePattern, Forwards) {
+	PatternSignature signature = IDA::build<"e4">();
+	auto hit = signature.next(bytesSpan.begin(), bytesSpan.end());
+
+	EXPECT_NE(hit, bytesSpan.end());
+	std::size_t offset = std::distance(bytesSpan.begin(), hit);
+	EXPECT_EQ(offset, 42);
+}
+
+TEST(BytePattern, Backwards) {
+	PatternSignature signature = IDA::build<"e4">();
+	auto hit = signature.prev(bytesSpan.rbegin(), bytesSpan.rend());
+
+	EXPECT_NE(hit, bytesSpan.rend());
+	std::size_t offset = std::distance(bytesSpan.rbegin(), hit);
+	EXPECT_EQ(offset, 26);
+}
+
+TEST(BytePattern, All) {
+	PatternSignature signature = IDA::build<"a9">();
+	std::vector<decltype(bytesSpan)::iterator> hits;
+	signature.all(bytesSpan.begin(), bytesSpan.end(), std::back_inserter(hits));
+
+	EXPECT_EQ(hits.size(), 3);
+	EXPECT_EQ(std::distance(bytesSpan.begin(), hits[0]), 51);
+	EXPECT_EQ(std::distance(bytesSpan.begin(), hits[1]), 91);
+	EXPECT_EQ(std::distance(bytesSpan.begin(), hits[2]), 97);
+}
+
+std::string string = "The Answer to the Great Question Of Life, the Universe and Everything Is Forty-two";
+std::span<std::byte> stringSpan{ reinterpret_cast<std::byte*>(string.data()), string.size() };
+
+TEST(StringPattern, Forwards) {
+	PatternSignature signature = String::build<"Forty-two", false>();
+	auto hit = signature.next(stringSpan.begin(), stringSpan.end());
+
+	EXPECT_NE(hit, stringSpan.end());
+	std::size_t offset = std::distance(stringSpan.begin(), hit);
+	EXPECT_EQ(offset, 73);
+}
+
+TEST(StringPattern, Backwards) {
+	PatternSignature signature = String::build<"Forty-two", false>();
+	auto hit = signature.prev(stringSpan.rbegin(), stringSpan.rend());
+
+	EXPECT_NE(hit, stringSpan.rend());
+	std::size_t offset = std::distance(stringSpan.rbegin(), hit);
+	EXPECT_EQ(offset, 0);
+}
+
+TEST(StringPattern, All) {
+	PatternSignature signature = String::build<" ?? ", false>();
+	std::vector<decltype(stringSpan)::iterator> hits;
+	signature.all(stringSpan.begin(), stringSpan.end(), std::back_inserter(hits));
+
+	EXPECT_EQ(hits.size(), 3);
+	EXPECT_EQ(std::distance(stringSpan.begin(), hits[0]), 10);
+	EXPECT_EQ(std::distance(stringSpan.begin(), hits[1]), 32);
+	EXPECT_EQ(std::distance(stringSpan.begin(), hits[2]), 69);
+}
+
+std::uintptr_t target = 0x13371337;
+
+std::uint8_t absoluteXRef[]{
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+
+	0x00, 0x00, 0x00, 0x00,
+#ifdef __x86_64
+	0x00, 0x00, 0x00, 0x00,
+#endif
+
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+};
+
+std::uint8_t relativeXRef[]{
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+
+	0x00, 0x00,
+#ifdef __x86_64
+	0x00, 0x00,
+#endif
+
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+};
+
+std::span<std::byte> absolutePadded{ reinterpret_cast<std::byte*>(absoluteXRef), sizeof(absoluteXRef) };
+std::span<std::byte> absoluteUnpadded{ reinterpret_cast<std::byte*>(absoluteXRef) + 8, sizeof(absoluteXRef) - 16 };
+std::span<std::byte> relativePadded{ reinterpret_cast<std::byte*>(relativeXRef), sizeof(relativeXRef) };
+std::span<std::byte> relativeUnpadded{ reinterpret_cast<std::byte*>(relativeXRef) + 8, sizeof(relativeXRef) - 16 };
+
+void initXRefArray() {
+	std::size_t offset = 8;
+
+	memcpy(absoluteXRef + offset, &target, sizeof(void*));
+
+	auto base = reinterpret_cast<std::uintptr_t>(relativeXRef + offset + 4);
+	auto pTarget = reinterpret_cast<std::uintptr_t>(&target);
+	std::size_t distance = std::max(pTarget, base) - std::min(pTarget, base);
+	auto jmpTarget = static_cast<std::int32_t>(distance);
+	if (base > pTarget)
+		jmpTarget *= -1;
+	std::memcpy(relativeXRef + offset, &jmpTarget, sizeof(std::int32_t));
+}
+
+TEST(XRefPattern, AbsoluteForwardsPadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<false, true>{ target };
+	auto hit = signature.next(absolutePadded.begin(), absolutePadded.end());
+
+	EXPECT_NE(hit, absolutePadded.end());
+	std::size_t offset = std::distance(absolutePadded.begin(), hit);
+	EXPECT_EQ(offset, 8);
+}
+
+TEST(XRefPattern, RelativeForwardsPadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<true, false>{ reinterpret_cast<std::uintptr_t>(&target) };
+	auto hit = signature.next(relativePadded.begin(), relativePadded.end());
+
+	EXPECT_NE(hit, relativePadded.end());
+	std::size_t offset = std::distance(relativePadded.begin(), hit);
+	EXPECT_EQ(offset, 8);
+}
+
+TEST(XRefPattern, AbsoluteForwardsUnpadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<false, true>{ target };
+	auto hit = signature.next(absoluteUnpadded.begin(), absoluteUnpadded.end());
+
+	EXPECT_NE(hit, absoluteUnpadded.end());
+	std::size_t offset = std::distance(absoluteUnpadded.begin(), hit);
+	EXPECT_EQ(offset, 0);
+}
+
+TEST(XRefPattern, RelativeForwardsUnpadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<true, false>{ reinterpret_cast<std::uintptr_t>(&target) };
+	auto hit = signature.next(relativeUnpadded.begin(), relativeUnpadded.end());
+
+	EXPECT_NE(hit, relativeUnpadded.end());
+	std::size_t offset = std::distance(relativeUnpadded.begin(), hit);
+	EXPECT_EQ(offset, 0);
+}
+
+TEST(XRefPattern, AbsoluteBackwardsPadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<false, true>{ target };
+	auto hit = signature.prev(absolutePadded.rbegin(), absolutePadded.rend());
+
+	EXPECT_NE(hit, absolutePadded.rend());
+	std::size_t offset = std::distance(absolutePadded.rbegin(), hit);
+	EXPECT_EQ(offset, 15);
+}
+
+TEST(XRefPattern, RelativeBackwardsPadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<true, false>{ reinterpret_cast<std::uintptr_t>(&target) };
+	auto hit = signature.prev(relativePadded.rbegin(), relativePadded.rend());
+
+	EXPECT_NE(hit, relativePadded.rend());
+	std::size_t offset = std::distance(relativePadded.rbegin(), hit);
+	EXPECT_EQ(offset, 11);
+}
+
+TEST(XRefPattern, AbsoluteBackwardsUnpadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<false, true>{ target };
+	auto hit = signature.prev(absoluteUnpadded.rbegin(), absoluteUnpadded.rend());
+
+	EXPECT_NE(hit, absoluteUnpadded.rend());
+	std::size_t offset = std::distance(absoluteUnpadded.rbegin(), hit);
+	EXPECT_EQ(offset, 7);
+}
+
+TEST(XRefPattern, RelativeBackwardsUnpadded) {
+	initXRefArray();
+
+	auto signature = XRefSignature<true, false>{ reinterpret_cast<std::uintptr_t>(&target) };
+	auto hit = signature.prev(relativeUnpadded.rbegin(), relativeUnpadded.rend());
+
+	EXPECT_NE(hit, relativeUnpadded.rend());
+	std::size_t offset = std::distance(relativeUnpadded.rbegin(), hit);
+	EXPECT_EQ(offset, 3);
+}
+
+TEST(XRefPattern, All) {
+	initXRefArray();
+
+	auto absoluteSig = XRefSignature<false, true>{ target };
+	auto relativeSig = XRefSignature<true, false>{ reinterpret_cast<std::uintptr_t>(&target) };
+	std::vector<decltype(absolutePadded)::iterator> hits;
+	absoluteSig.all(absolutePadded.begin(), absolutePadded.end(), std::back_inserter(hits));
+	relativeSig.all(relativePadded.begin(), relativePadded.end(), std::back_inserter(hits));
+
+	EXPECT_EQ(hits.size(), 2);
+	EXPECT_EQ(reinterpret_cast<void*>(&*hits[0]), absoluteXRef + 8);
+	EXPECT_EQ(reinterpret_cast<void*>(&*hits[1]), relativeXRef + 8);
+}
--- Include/SignatureScanner.hpp
+++ Include/SignatureScanner.hpp
@@ -1,114 +1,511 @@
 #ifndef SIGNATURESCANNER_HPP
 #define SIGNATURESCANNER_HPP
 
+#include <algorithm>
+#include <array>
+#include <cstdint>
 #include <optional>
 #include <string>
+#include <variant>
 #include <vector>
-#include <cstdint>
 
 namespace SignatureScanner {
+	namespace detail {
+		using InnerPatternElement = std::byte;
+		using PatternElement = std::optional<InnerPatternElement>;
 
-	class Signature {
-		[[nodiscard]] virtual std::optional<std::uintptr_t> prev(std::uintptr_t begin, std::optional<std::uintptr_t> end) const = 0;
-		[[nodiscard]] virtual std::optional<std::uintptr_t> next(std::uintptr_t begin, std::optional<std::uintptr_t> end) const = 0;
-		[[nodiscard]] virtual std::vector<std::uintptr_t> all(std::uintptr_t begin, std::uintptr_t end) const = 0;
-
-	public:
-		virtual ~Signature() = default;
-
-		template <typename R = std::uintptr_t, typename T, typename T2 = std::uintptr_t>
-		[[nodiscard]] inline std::optional<R> findPrev(T begin, std::optional<T2> end = std::nullopt) const
+		template <typename T>
+		constexpr auto patternCompare(const T& byte, const detail::PatternElement& elem)
 		{
-			auto res = prev(std::uintptr_t(begin), end.has_value() ? std::optional{std::uintptr_t(end.value())} : std::nullopt);
-			if(res.has_value())
-				return R(res.value());
-			return std::nullopt;
+			return !elem.has_value() || elem.value() == byte;
 		}
 
-		template <typename R = std::uintptr_t, typename T, typename T2 = std::uintptr_t>
-		[[nodiscard]] inline std::optional<R> findNext(T begin, std::optional<T2> end = std::nullopt) const
-		{
-			auto res = next(std::uintptr_t(begin), end.has_value() ? std::optional{std::uintptr_t(end.value())} : std::nullopt);
-			if(res.has_value())
-				return R(res.value());
-			return std::nullopt;
-		}
+		/**
+		 * This thing is pretty much useless outside of this use-case,
+		 * because the array could be partially filled up when we get it,
+		 * for our use-case this doesn't matter.
+		 */
+		template <typename T, std::size_t N>
+		struct ArrayInserter {
+			std::array<T, N>& array;
+			std::size_t idx = 0;
 
-		template <typename R = std::uintptr_t, typename T, typename T2 = std::uintptr_t>
-		[[nodiscard]] inline std::vector<R> findAll(T begin, T2 end) const
-		{
-			std::vector<std::uintptr_t> vector = all(std::uintptr_t(begin), std::uintptr_t(end));
-			if constexpr (std::is_convertible_v<decltype(vector), std::vector<R>>)
-				return vector;
-			else { // when you are using this path then you are enduring a performance hit because the vector has to be copied first
-				std::vector<R> newVector{};
-				for (std::uintptr_t v : vector) {
-					newVector.push_back(R(v));
+			constexpr explicit ArrayInserter(std::array<T, N>& array)
+				: array(array)
+			{
+			}
+
+			constexpr ArrayInserter& operator=(T obj)
+			{
+				array[idx] = obj;
+				idx++;
+				return *this;
+			}
+
+			constexpr ArrayInserter& operator*()
+			{
+				return *this;
+			}
+			constexpr ArrayInserter& operator++()
+			{
+				return *this;
+			}
+			constexpr ArrayInserter& operator++(int)
+			{
+				return *this;
+			}
+		};
+
+		template <std::size_t N>
+		struct TemplateString : std::array<char, N> {
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "google-explicit-constructor"
+			constexpr TemplateString(const char (&str)[N + 1])
+				: std::array<char, N>()
+			{
+				std::copy(std::begin(str), std::end(str) - 1, ArrayInserter<char, N>(*this));
+			}
+#pragma clang diagnostic pop
+		};
+
+		template <std::size_t N>
+		TemplateString(const char (&str)[N]) -> TemplateString<N - 1>;
+	}
+
+	namespace IDA {
+		namespace ida_detail {
+			constexpr uint8_t chrToHex(char c)
+			{
+				if ('0' <= c && c <= '9') {
+					return c - '0';
+				} else if ('A' <= c && c <= 'F') {
+					return c - 'A' + 10;
+				} else if ('a' <= c && c <= 'f') {
+					return c - 'a' + 10;
 				}
-				return newVector;
+				return 0;
+			}
+
+			constexpr uint8_t strToHex(std::string_view input)
+			{
+				uint8_t val = 0;
+				for (char c : input) {
+					val *= 16;
+					val += chrToHex(c);
+				}
+				return val;
+			}
+
+			template <detail::TemplateString string>
+			constexpr std::size_t countWords(char delimiter)
+			{
+				bool wasChar = false;
+
+				std::size_t count = 0;
+				for (char c : string) {
+					bool isChar = c != delimiter;
+					if (!wasChar && isChar)
+						count++;
+
+					wasChar = c != delimiter;
+				}
+
+				return count;
+			}
+
+			constexpr detail::PatternElement buildWord(std::string_view word, char wildcard)
+			{
+				if (std::ranges::all_of(word, [wildcard](char c) { return c == wildcard; }))
+					return std::nullopt;
+				else
+					return static_cast<detail::InnerPatternElement>(strToHex(word));
+			}
+
+			template <typename Range, typename Inserter>
+			constexpr void buildSignature(const Range& range, Inserter inserter, char Delimiter, char Wildcard)
+			{
+				std::string word;
+
+				std::size_t idx = 0;
+				for (char c : range) {
+					if (c == Delimiter) {
+						if (word.empty())
+							continue;
+
+						inserter = buildWord(word, Wildcard);
+						idx++;
+
+						word = "";
+					} else
+						word += c;
+				}
+
+				if (!word.empty()) {
+					inserter = buildWord(word, Wildcard);
+				}
+			}
+		}
+
+		const char DEFAULT_DELIMITER = ' ';
+		const char DEFAULT_WILDCARD = '?';
+
+		template <detail::TemplateString String, char Delimiter = DEFAULT_DELIMITER, char Wildcard = DEFAULT_WILDCARD, std::size_t Length = ida_detail::countWords<String>(Delimiter)>
+		consteval auto build()
+		{
+			std::array<detail::PatternElement, Length> signature;
+
+			ida_detail::buildSignature(String, detail::ArrayInserter(signature), Delimiter, Wildcard);
+
+			return signature;
+		}
+
+		constexpr auto build(std::string_view string, char delimiter = DEFAULT_DELIMITER, char wildcard = DEFAULT_WILDCARD)
+		{
+			std::vector<detail::PatternElement> signature;
+
+			ida_detail::buildSignature(string, std::back_inserter(signature), delimiter, wildcard);
+
+			return signature;
+		}
+	}
+
+	namespace String {
+		const char DEFAULT_WILDCARD = '?';
+
+		template <detail::TemplateString String, bool IncludeTerminator = true, char Wildcard = DEFAULT_WILDCARD>
+		consteval auto build()
+		{
+			std::array<detail::PatternElement, IncludeTerminator ? String.size() + 1 : String.size()> signature;
+
+			for (size_t i = 0; i < String.size(); i++) {
+				if (String[i] == Wildcard)
+					signature[i] = std::nullopt;
+				else
+					signature[i] = static_cast<detail::InnerPatternElement>(String[i]);
+			}
+
+			if constexpr (IncludeTerminator) {
+				signature[signature.size() - 1] = (static_cast<detail::InnerPatternElement>('\0'));
+			}
+
+			return signature;
+		}
+
+		constexpr auto build(std::string_view string, bool includeTerminator = true, char wildcard = DEFAULT_WILDCARD)
+		{
+			std::vector<detail::PatternElement> signature;
+			signature.reserve(string.size() + (includeTerminator ? 1 : 0));
+
+			for (char c : string) {
+				if (c == wildcard)
+					signature.emplace_back(std::nullopt);
+				else
+					signature.emplace_back(static_cast<detail::InnerPatternElement>(c));
+			}
+
+			if (includeTerminator) {
+				signature.emplace_back(static_cast<detail::InnerPatternElement>('\0'));
+			}
+
+			return signature;
+		}
+	}
+
+	template <typename Derived>
+	class Signature {
+	public:
+		template <typename Iter, typename Sent, typename Inserter>
+		[[nodiscard]] constexpr auto all(Iter begin, const Sent& end, Inserter inserter) const
+		{
+			while (true) {
+				auto it = static_cast<const Derived*>(this)->next(begin, end);
+				if (it == end)
+					break;
+				*inserter = it;
+				begin = it;
+				begin++;
 			}
 		}
 	};
 
-	class PatternSignature : public Signature {
+	template <std::size_t N>
+	class PatternSignature : public Signature<PatternSignature<N>> {
 	private:
-		[[nodiscard]] std::optional<std::uintptr_t> prev(std::uintptr_t begin, std::optional<std::uintptr_t> end) const override;
-		[[nodiscard]] std::optional<std::uintptr_t> next(std::uintptr_t begin, std::optional<std::uintptr_t> end) const override;
-		[[nodiscard]] std::vector<std::uintptr_t> all(std::uintptr_t begin, std::uintptr_t end) const override;
+		std::array<detail::PatternElement, N> elements;
 
 	public:
-		using Element = std::optional<std::byte>;
-
-	protected:
-		std::vector<Element> elements;
-
-	public:
-		explicit PatternSignature(std::vector<PatternSignature::Element> elements);
-
-		[[nodiscard]] std::size_t length() const;
-
-		[[nodiscard]] bool doesMatch(std::uintptr_t addr) const;
-
-		template <typename T = std::uintptr_t>
-		[[nodiscard]] bool doesMatch(T addr) const
+		constexpr PatternSignature(std::array<detail::PatternElement, N>&& elements)
+			: elements(std::move(elements))
 		{
-			return doesMatch(std::uintptr_t(addr));
+		}
+
+		[[nodiscard]] constexpr const std::vector<detail::PatternElement>& getElements() const { return elements; }
+
+		template <typename Iter, std::sentinel_for<Iter> Sent>
+		[[nodiscard]] constexpr auto next(const Iter& begin, const Sent& end) const
+		{
+			return std::ranges::search(begin, end, elements.cbegin(), elements.cend(), detail::patternCompare<decltype(*std::declval<Iter>())>).begin();
+		}
+
+		template <typename Iter, std::sentinel_for<Iter> Sent>
+		[[nodiscard]] constexpr auto prev(const Iter& begin, const Sent& end) const
+		{
+			return std::ranges::search(begin, end, elements.crbegin(), elements.crend(), detail::patternCompare<decltype(*std::declval<Iter>())>).begin();
+		}
+
+		template <typename Iter>
+		[[nodiscard]] constexpr bool doesMatch(const Iter& addr) const
+		{
+			return std::equal(elements.cbegin(), addr.cbegin(), elements.cend(), std::next(addr.cbegin(), elements.length()), detail::patternCompare<decltype(*std::declval<Iter>())>);
 		}
 	};
 
-	class StringSignature : public PatternSignature {
-	public:
-		explicit StringSignature(const std::string& string, std::optional<char> wildcard = std::nullopt);
-	};
+	namespace detail {
+		template <std::size_t N>
+		struct StaticQueue : std::array<std::byte, N> {
+			using std::array<std::byte, N>::array;
 
-	class ByteSignature : public PatternSignature {
-	public:
-		// IDA style
-		explicit ByteSignature(const std::string& bytes, char wildcard = '?');
+			constexpr StaticQueue(std::array<std::byte, N> arr)
+				: std::array<std::byte, N>(arr)
+			{
+			}
 
-		// Code style
-		ByteSignature(const char* bytes, const std::string& mask, char maskChar = 'x' /*defines the char which enables a byte, not the one which disables one*/);
-	};
+			constexpr void push_front(std::byte b)
+			{
+				for (std::size_t i = N - 1; i > 0; i--)
+					(*this)[i] = (*this)[i - 1];
+				(*this)[0] = b;
+			}
 
-	class XRefSignature : public Signature {
-		const std::uintptr_t address;
-		const bool relativeReferences;
-		const bool absoluteReferences;
+			constexpr void push_back(std::byte b)
+			{
+				for (std::size_t i = 1; i < N; i++)
+					(*this)[i - 1] = (*this)[i];
+				(*this)[N - 1] = b;
+			}
 
-		[[nodiscard]] std::optional<std::uintptr_t> prev(std::uintptr_t begin, std::optional<std::uintptr_t> end) const override;
-		[[nodiscard]] std::optional<std::uintptr_t> next(std::uintptr_t begin, std::optional<std::uintptr_t> end) const override;
-		[[nodiscard]] std::vector<std::uintptr_t> all(std::uintptr_t begin, std::uintptr_t end) const override;
+			template <std::size_t NewN>
+			constexpr std::array<std::byte, NewN> sliced(std::size_t offset = 0)
+				requires(NewN <= N)
+			{
+				std::array<std::byte, NewN> arr;
+				for (std::size_t i = 0; i < NewN; i++)
+					arr[i] = (*this)[offset + i];
+				return arr;
+			}
+		};
 
-	public:
-		explicit XRefSignature(std::uintptr_t address, bool relativeReferences = true, bool absoluteReferences = true);
-		explicit XRefSignature(const void* address, bool relativeReferences = true, bool absoluteReferences = true);
-
-		[[nodiscard]] bool doesMatch(std::uintptr_t addr, std::size_t space) const;
-
-		template <typename T = std::uintptr_t>
-		[[nodiscard]] bool doesMatch(T addr, std::size_t space /* relative (2 or 4) and absolute (4 or 8) references have different lengths */) const
+		template <typename T, std::size_t N>
+		constexpr T convertBytes(const std::array<std::byte, N> array, std::endian endianness)
 		{
-			return doesMatch(std::uintptr_t(addr), space);
+			T num = std::bit_cast<T>(array);
+			if (endianness != std::endian::native)
+				num = std::byteswap(num);
+			return num;
+		}
+	}
+
+	template <bool Relative = true, bool Absolute = true, std::endian Endianness = std::endian::native>
+	class XRefSignature : public Signature<XRefSignature<Relative, Absolute, Endianness>> {
+		static_assert(Relative || Absolute);
+
+		using RelAddrType = std::conditional_t<sizeof(void*) == 8, std::int32_t, std::int16_t>;
+		const std::uintptr_t address;
+		[[no_unique_address]] const std::conditional_t<Relative, std::size_t, std::monostate> instructionLength;
+		static constexpr std::size_t MAX_SLIDE_WINDOW_SIZE = Relative ? std::max(sizeof(std::uintptr_t), sizeof(RelAddrType)) : sizeof(std::uintptr_t);
+		static constexpr std::size_t REL_OFFSET_SIZE = sizeof(RelAddrType);
+
+	public:
+		explicit constexpr XRefSignature(std::uintptr_t address, std::size_t instructionLength)
+			requires Relative
+			: address(address)
+			, instructionLength(instructionLength)
+		{
+		}
+
+		explicit constexpr XRefSignature(std::uintptr_t address)
+			: address(address)
+			, instructionLength([] {
+				if constexpr (Relative)
+					return sizeof(RelAddrType);
+				else
+					return decltype(instructionLength){};
+			}())
+		{
+		}
+
+	private:
+		template <typename Iter, std::sentinel_for<Iter> Sent, bool Backwards>
+		constexpr auto twoPointer(const Iter& begin, const Sent& end) const
+		{
+			auto range = std::ranges::subrange{ begin, end };
+
+			detail::StaticQueue<MAX_SLIDE_WINDOW_SIZE> number{};
+
+			auto p1 = begin;
+			auto p2 = begin;
+
+			std::size_t i = 0;
+			std::size_t size = std::min(MAX_SLIDE_WINDOW_SIZE, static_cast<decltype(MAX_SLIDE_WINDOW_SIZE)>(std::distance(begin, end)));
+			while (true) {
+				if constexpr (Backwards) {
+					if constexpr (Relative) {
+						if constexpr (Endianness == std::endian::little) {
+							number.push_front(*p2);
+						} else {
+							number.push_back(*p2);
+						}
+
+						if (i >= REL_OFFSET_SIZE - 1) {
+							auto offset = detail::convertBytes<RelAddrType>(number.template sliced<REL_OFFSET_SIZE>(), Endianness);
+							if (doesRelativeMatch(offset, reinterpret_cast<std::uintptr_t>(&*p2))) {
+								return p2;
+							}
+						}
+					} else // faster
+						number[size - 1 - i] = *p2;
+				} else {
+					number[i] = *p2;
+				}
+
+				i++;
+
+				if (i >= size)
+					break;
+
+				p2++;
+			}
+
+			if (size == MAX_SLIDE_WINDOW_SIZE)
+				while (true) {
+					auto offset = detail::convertBytes<std::uintptr_t>(number, Endianness);
+
+					if constexpr (Absolute) {
+						if (doesAbsoluteMatch(offset)) {
+							if constexpr (Backwards) {
+								return p2;
+							} else {
+								return p1;
+							}
+						}
+					}
+
+					if constexpr (Relative) {
+						if constexpr (Backwards) {
+							if (doesRelativeMatch(static_cast<RelAddrType>(offset), reinterpret_cast<std::uintptr_t>(&*p2)))
+								return p2;
+						} else {
+							if (doesRelativeMatch(static_cast<RelAddrType>(offset), reinterpret_cast<std::uintptr_t>(&*p1)))
+								return p1;
+						}
+					}
+
+					p2++;
+
+					if (p2 == end)
+						break;
+
+					p1++;
+
+					if constexpr (Backwards == (Endianness == std::endian::little))
+						number.push_front(*p2);
+					else
+						number.push_back(*p2);
+				}
+
+			if constexpr (!Backwards && Relative && REL_OFFSET_SIZE < MAX_SLIDE_WINDOW_SIZE) {
+				i = 0;
+				while (i <= size - REL_OFFSET_SIZE) {
+					auto offset = detail::convertBytes<RelAddrType>(number.template sliced<REL_OFFSET_SIZE>(i), Endianness);
+
+					if (doesRelativeMatch(offset, reinterpret_cast<std::uintptr_t>(&*p1)))
+						return p1;
+
+					p1++;
+					i++;
+				}
+			}
+
+			return end;
+		}
+
+	public:
+		template <typename Iter, typename Sent>
+		[[nodiscard]] constexpr auto next(const Iter& begin, const Sent& end) const
+		{
+			return twoPointer<Iter, Sent, false>(begin, end);
+		}
+
+		template <typename Iter, typename Sent>
+		[[nodiscard]] constexpr auto prev(const Iter& begin, const Sent& end) const
+		{
+			return twoPointer<Iter, Sent, true>(begin, end);
+		}
+
+		template <typename Iter, std::sentinel_for<Iter> Sent = std::unreachable_sentinel_t>
+		[[nodiscard]] constexpr bool doesMatch(const Iter& addr, const Sent& end = std::unreachable_sentinel_t{}) const
+		{
+			detail::StaticQueue<MAX_SLIDE_WINDOW_SIZE> number;
+			std::size_t i = 0;
+			std::uintptr_t location;
+			for (auto it = addr; it != end; it++) {
+				if constexpr (Endianness == std::endian::little)
+					number.push_back(*it);
+				else
+					number.push_front(*it);
+				i++;
+
+				location = detail::convertBytes<std::uintptr_t>(number, Endianness);
+
+				if constexpr (Relative) {
+					if (i >= REL_OFFSET_SIZE && doesRelativeMatch(static_cast<RelAddrType>(number), location)) {
+						return true;
+					}
+				}
+
+				if (i >= MAX_SLIDE_WINDOW_SIZE)
+					break;
+			}
+
+			if constexpr (Absolute) {
+				if (i >= MAX_SLIDE_WINDOW_SIZE && doesAbsoluteMatch(location)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		[[nodiscard]] constexpr bool doesMatch(std::uintptr_t number, std::conditional_t<Relative, std::uintptr_t, std::monostate> location = {}) const
+		{
+			if constexpr (Absolute) {
+				if (doesAbsoluteMatch(number)) {
+					return true;
+				}
+			}
+
+			if constexpr (Relative) {
+				if (doesRelativeMatch(static_cast<RelAddrType>(number), location)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		[[nodiscard]] constexpr bool doesAbsoluteMatch(std::uintptr_t number) const
+			requires Absolute
+		{
+			return number == address;
+		}
+
+		[[nodiscard]] constexpr bool doesRelativeMatch(RelAddrType offset, std::uintptr_t location) const
+			requires Relative
+		{
+			// This is a special which I encountered if the address has 0x00 bytes before it, thus it becomes location + instructionLength + 0.
+			// I can't imagine a case in which this is desired.
+			if (offset == 0)
+				return false;
+			return location + instructionLength + offset == address;
 		}
 	};
 }
